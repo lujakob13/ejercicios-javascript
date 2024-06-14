9f81592ed68b103cf550b1e8c6d02475f3ede711
luca jakobson <luca081104@gmail.com>
2024-06-14 20:14:22 -0300

    realize la actividad 3, comentando que hace cada linea de codigo

---
+++ js/ejercicio3.js
@@ -0,0 +1,21 @@
+// creo una variable para un numero
+let numero = 12
+// creo la funcion
+function propiedades(valor){
+    // calculo el cuadrado
+    let cuadrado = Math.pow(valor, 2)
+    // calculo el valor absoluto
+    let ValorAbsoluto = Math.abs(valor)
+    // calculo la raiz cuadrada
+    let raiz = Math.sqrt(valor)
+    // creo el objeto
+    return{
+        cuadrado: cuadrado,
+        raiz: raiz,
+        ValorAbsoluto: ValorAbsoluto
+    }
+}
+// guardo el resultado en una variable
+let resultado = propiedades(numero)
+// templete string
+console.log(`el cuadrado es ${resultado.cuadrado}, la raiz es ${resultado.raiz} y el valor abosluto es ${resultado.ValorAbsoluto}`)
