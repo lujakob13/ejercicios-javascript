4b911095a87fb49bbde3106d384e78606a2710f1
luca jakobson <luca081104@gmail.com>
2024-06-15 12:11:05 -0300

    descomente la actividad 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,13 +1,13 @@
-// let numeros = [1,2,3,4,5,6,7,8,9,45,35,23,47, 67, 88, 81]
-// // creo la funcion que recibe como parametro el array "numeros"
-// function cuadrado(array){
-//     // creo la variable resultado, que es un array vacio
-//     let resultado= []
-//     // hago el for of
-//     for(numero of array){
-//         // utilizo push para que me agregue los numeros al array resultado
-//         resultado.push(numero * numero)   
-//     }//retorno la varibale resultado
-//     return resultado
-// }
-// console.log(`cuadrado de cada numero ${cuadrado(numeros)}`)
+let numeros = [1,2,3,4,5,6,7,8,9,45,35,23,47, 67, 88, 81]
+// creo la funcion que recibe como parametro el array "numeros"
+function cuadrado(array){
+    // creo la variable resultado, que es un array vacio
+    let resultado= []
+    // hago el for of
+    for(numero of array){
+        // utilizo push para que me agregue los numeros al array resultado
+        resultado.push(numero * numero)   
+    }//retorno la varibale resultado
+    return resultado
+}
+console.log(`cuadrado de cada numero ${cuadrado(numeros)}`)
